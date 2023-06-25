66e90af7ec9b3ba5e34d8c00a71c86c87fe94541
Peter Graham <peterghm@gmail.com>
2023-06-25 18:14:09 -0400

    try to show immersive graph (works in preview but not actual app)

--- StonksPro/ImmersiveView.swift
+++ StonksPro/ImmersiveView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import Charts
 import RealityKit
 import RealityKitContent
 
+let cheeseburgerCost: [Food] = [
+    .init(name: "Cheeseburger", price: 0.15, year: 1960),
+    .init(name: "Cheeseburger", price: 0.20, year: 1970),
+    // ...
+    .init(name: "Cheeseburger", price: 1.10, year: 2020)
+]
+
+
+struct Food: Identifiable {
+    let name: String
+    let price: Double
+    let date: Date
+    let id = UUID()
+
+
+    init(name: String, price: Double, year: Int) {
+        self.name = name
+        self.price = price
+        let calendar = Calendar.autoupdatingCurrent
+        self.date = calendar.date(from: DateComponents(year: year))!
+    }
+}
+
 struct ImmersiveView: View {
     var body: some View {
-        RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(scene)
-            }
+        Chart(cheeseburgerCost) { cost in
+            AreaMark(
+                x: .value("Date", cost.date),
+                y: .value("Price", cost.price)
+            )
         }
     }
 }
